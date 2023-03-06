cdd2fc8641a5fd4e5df5079ae13bed04a6ef7d61
HoangLong253 <0306201253@caothang.edu.vn>
2023-03-06 10:59:58 +0700

    cập nhật

--- for mysql.sql
+++ for mysql.sql
@@ -1,45 +1,43 @@
 create table LoaiSach
 (
-	MaLoaiSach int,
-	TenLoaiSach nvarchar(30),
+	MaLoaiSach varchar(2),
+	TenLoaiSach varchar(30),
 	updated_at TIMESTAMP,
 
-	constraint PK_LoaiSach primary key (MaLoaiSach),
-)
-
-go
+	constraint PK_LoaiSach primary key (MaLoaiSach)
+);
 
 create table Sach
 (
 	MaSach varchar(10),
-	MaNXB int,
-	MaLoaiSach int,
-	MaGiamGia varchar(5),
-	TenSach nvarchar(50),
-	MoTa nvarchar(MAX),
-	HinhAnh varchar(MAX),
+	MaNXB varchar(2),
+	MaLoaiSach varchar(2),
+	MaGiamGia varchar(10),
+	TenSach varchar(50),
+	MoTa varchar(255),
+	HinhAnh varchar(255),
 	DonGia int,
 	TrangThai bit,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_Sach primary key (MaSach),
-)
+	constraint PK_Sach primary key (MaSach)
+);
 
 create table NhaXuatBan
 (
-	MaNXB int,
-	TenNXB nvarchar(25),
+	MaNXB varchar(2),
+	TenNXB varchar(25),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_NhaXuatBan primary key (MaNXB),
-)
+	constraint PK_NhaXuatBan primary key (MaNXB)
+);
 
 create table NguoiDung
 (
 	MaNgDung varchar(5),
 	MaGioHang varchar(10),
-	HoTen nvarchar(50), 
+	HoTen varchar(50), 
 	Email varchar(30),
 	SDT varchar(10),
 	TrangThai bit,
@@ -47,13 +45,13 @@ create table NguoiDung
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_NguoiDung primary key (MaNgDung),
-)
+	constraint PK_NguoiDung primary key (MaNgDung)
+);
 
 create table NhanVien
 (
 	MaNV varchar(5),
-	HoTen nvarchar(50), 
+	HoTen varchar(50), 
 	Email varchar(30),
 	SDT varchar(10),
 	TrangThai bit,
@@ -61,14 +59,14 @@ create table NhanVien
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_NhanVien primary key (MaNV),
-)
+	constraint PK_NhanVien primary key (MaNV)
+);
 
 create table NgGiaoHang
 (
 	MaNgGiaoHang varchar(5),
-	MaHDBan int,
-	HoTen nvarchar(50), 
+	MaHDBan varchar(5),
+	HoTen varchar(50), 
 	Email varchar(30),
 	SDT varchar(10),
 	TrangThai bit,
@@ -76,190 +74,194 @@ create table NgGiaoHang
 	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_GiaoHang primary key (MaNgGiaoHang),
-)
+	constraint PK_GiaoHang primary key (MaNgGiaoHang)
+);
 
 create table HoaDonBan
 (
-	MaHDBan int,
+	MaHDBan varchar(5),
 	MaNV varchar(5),
 	MaNgDung varchar(5),
-	MaNgGiaoHang varchar(5),
+	MaNgGiaoHang varchar(10),
 	TongTien int,
 	TrangThaiGiaoHang int,
 	TrangThaiThanhToan bit,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_HoaDonBan primary key (MaHDBan),
-)
+	constraint PK_HoaDonBan primary key (MaHDBan)
+);
 
 create table CTHoaDonBan
 (
-	MaHDBan int,
+	MaCTHDB int NOT NULL AUTO_INCREMENT,
+	MaHDBan varchar(5),
 	MaSach varchar(10),
 	Sluong int,
 	GiaTien int,
-)
+
+	constraint PK_CTHoaDonBan primary key (MaCTHDB)
+);
 
 create table HoaDonNhap
 (
-	MaHDNhap int,
+	MaHDNhap varchar(5),
 	MaNV varchar(5),
 	TongTien int,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_HoaDonNhap primary key (MaHDNhap),
-)
+	constraint PK_HoaDonNhap primary key (MaHDNhap)
+);
 
 create table CTHoaDonNhap
 (
 	MaCTHDN int NOT NULL AUTO_INCREMENT,
-	MaHDNhap int,
+	MaHDNhap varchar(5),
 	MaSach varchar(10),
 	Sluong int,
 	GiaTien int,
 
 	constraint PK_Kho primary key(MaCTHDN)
-)
+);
 
 create table Kho 
 (
 	MaKho varchar(5),
-	MaHDNhap int,
+	MaHDNhap varchar(5),
 	MaSach varchar(10),
 	Sluong int,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-	constraint PK_Kho primary key (MaKho),
-)
+	constraint PK_Kho primary key (MaKho)
+);
 
 create table GiamGia
 (
-	MaGiamGia varchar(5),
+	MaGiamGia varchar(10) UNIQUE,
 	PhanTramGiam float,
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at datetime,
 
 	constraint PK_GiamGia primary key (MaGiamGia)
-)
+);
 
 create table DSGiamGia
 (
 	MaDSGGia int NOT NULL AUTO_INCREMENT,
 	MaNgDung varchar(5),
-	MaGiamGia varchar(5),
+	MaGiamGia varchar(10),
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	deleted_at datetime,
 
 	constraint PK_DSGiamGia primary key (MaDSGGia)
-)
+);
 
 create table GioHang
 (
 	MaGioHang varchar(10),
-	SL int, 
+	MaNgDung varchar(5),
 	TongTien int,
-	updated_at TIMESTAMP,
-	deleted_at TIMESTAMP,
+	updated_at datetime,
+	deleted_at datetime,
 
-	constraint PK_GioHang primary key (MaGioHang),
-)
+	constraint PK_GioHang primary key (MaGioHang)
+);
 
 create table CTGioHang
 (
 	MaCTGHang int NOT NULL AUTO_INCREMENT,
 	MaGioHang varchar(10),
 	MaSach varchar(10),
+	SL int,
 	GiaTien int,
+	Tong int,
 	updated_at TIMESTAMP,
-	deleted_at TIMESTAMP,
+	deleted_at datetime,
 
-	constraint PK_CTGHang primary key (MaCTGHang),
-)
-
-go
+	constraint PK_CTGHang primary key (MaCTGHang)
+);
 
 alter table Sach
 add constraint FK_Sach_LoaiSach
 foreign key (MaLoaiSach)
-references LoaiSach(MaLoaiSach)
+references LoaiSach(MaLoaiSach);
 
 alter table Sach
 add constraint FK_Sach_NhaXuatBan
 foreign key (MaNXB)
-references NhaXuatBan(MaNXB)
+references NhaXuatBan(MaNXB);
 
 alter table Sach
 add constraint FK_Sach_GiamGia
 foreign key (MaGiamGia)
-references GiamGia(MaGiamGia)
+references GiamGia(MaGiamGia);
 
 alter table CTHoaDonBan
 add constraint FK_CTHoaDonBan_HoaDonBan
 foreign key (MaHDBan)
-references HoaDonBan(MaHDBan)
+references HoaDonBan(MaHDBan);
 
 alter table CTHoaDonBan
 add constraint CTHoaDonBan_Sach
 foreign key (MaSach)
-references Sach(MaSach)
+references Sach(MaSach);
 
 alter table CTHoaDonNhap
 add constraint FK_CTHoaDonNhap_HoaDonNhap
 foreign key (MaHDNhap)
-references HoaDonNhap(MaHDNhap)
+references HoaDonNhap(MaHDNhap);
 
 alter table CTHoaDonNhap
 add constraint FK_CTHoaDonNhap_Sach
 foreign key (MaSach)
-references Sach(MaSach)
+references Sach(MaSach);
 
 alter table Kho
 add constraint FK_Kho_Sach
 foreign key (MaSach)
-references Sach(MaSach)
+references Sach(MaSach);
 
 alter table Kho
 add constraint FK_Kho_HoaDonNhap
 foreign key (MaHDNhap)
-references HoaDonNhap(MaHDNhap)
+references HoaDonNhap(MaHDNhap);
 
 alter table NguoiDung
 add constraint FK_NguoiDung_GioHang
 foreign key (MaGioHang)
-references GioHang(MaGioHang)
+references GioHang(MaGioHang);
 
 alter table HoaDonNhap
 add constraint HoaDonNhap_NhanVien
 foreign key (MaNV)
-references NhanVien(MaNV)
+references NhanVien(MaNV);
 
 alter table HoaDonBan
 add constraint HoaDonBan_NhanVien
 foreign key (MaNV)
-references NhanVien(MaNV)
+references NhanVien(MaNV);
 
 alter table HoaDonBan
 add constraint FK_HoaDonBan_NgGiaoHang
 foreign key (MaNgGiaoHang)
-references NgGiaoHang(MaNgGiaoHang)
+references NgGiaoHang(MaNgGiaoHang);
 
 alter table CTGioHang
 add constraint FK_CTGioHang_GioHang
 foreign key (MaGioHang)
-references GioHang(MaGioHang)
+references GioHang(MaGioHang);
 
 alter table DSGiamGia
 add constraint FK_DSGiamGia_NgDung
 foreign key (MaNgDung)
-references NguoiDung(MaNgDung)
+references NguoiDung(MaNgDung);
 
 alter table DSGiamGia
 add constraint FK_DSGiamGia_GiamGia
 foreign key (MaGiamGia)
-references GiamGia(MaGiamGia)
+references GiamGia(MaGiamGia);
 
---NhanVien
+--insert into
+
 insert into 
 NhanVien(MaNV, HoTen, Email, SDT, TrangThai, created_at, updated_at, deleted_at) 
 values ('NV01', N'Nguyễn Văn A', 'NgVA@gmail.com', '0891234567', 1, CURRENT_TIMESTAMP, '', '');
@@ -272,68 +274,81 @@ insert into
 NhanVien(MaNV, HoTen, Email, SDT, TrangThai, created_at, updated_at, deleted_at) 
 values ('NV03', N'Trần Kha C', 'NgVA@gmail.com', '0891234567', 1, CURRENT_TIMESTAMP, '', '');
 
---GioHang
 INSERT into
 GioHang(MaGioHang, MaNgDung, TongTien, updated_at, deleted_at)
 VALUES('GIOH01', 'ND01', 0, '', '');
 
---NguoiDung
 insert into
 NguoiDung(MaNgDung, MaGioHang, HoTen, Email, SDT, TrangThai, created_at, updated_at, deleted_at)
 values ('ND01', 'GIOH01', N'Hoàng C', 'NC@gmail.com', '0891234567', 1, CURRENT_TIMESTAMP, '', '');
 
---NgGiaoHang
 insert into
 NgGiaoHang(MaNgGiaoHang, MaHDBan, HoTen, Email, SDT, TrangThai, created_at, updated_at, deleted_at)
 values ('GIAOH01', 'HDB01', 'Trần Thị D', 'T2D@gmail.com', '0891234567', 1, CURRENT_TIMESTAMP, '', '');
 
---CTGioHang
 INSERT into 
 CTGioHang(MaGioHang, MaSach, SL, GiaTien, Tong, updated_at, deleted_at)
 VALUES('GIOH01', 'S0001', 2, 6000, 0, '', '');
 
---LoaiSach
 insert into
 LoaiSach(MaLoaiSach, TenLoaiSach, updated_at)
 values('GK', N'Sách giáo khoa', '');
 
---NhaXuatBan
 insert into
 NhaXuatBan(MaNXB, TenNXB, created_at)
 values('GD', N'Giáo Dục', CURRENT_TIMESTAMP);
 
---Sach
 insert into
 Sach(MaSach, MaNXB, MaLoaiSach, MaGiamGia, TenSach, MoTa, HinhAnh, DonGia, TrangThai, created_at, updated_at)
 values ('S0001', 'GD', 'GK', null, N'Ngữ văn 6 tập 1', '', '', 6000, 1, CURRENT_TIMESTAMP, '');
 
---Kho
-
---HoaDonNhap
 insert into
 HoaDonNhap(MaHDNhap, MaNV, TongTien, created_at)
 values('HDN01', 'NV01', 60000, CURRENT_TIMESTAMP);
 
---HoaDonBan
 insert into
 HoaDonBan(MaHDBan, MaNV, TongTien, created_at)
 values('HDB01', 'NV03', 60000, CURRENT_TIMESTAMP);
 
---CTHoaDonNhap
 insert into
 CTHoaDonNhap(MaHDNhap, MaSach, Sluong, GiaTien)
 values('HDN01', 'S0001', 10, 6000);
 
---CTHoaDonBan
 insert into
 CTHoaDonBan(MaHDBan, MaSach, Sluong, GiaTien)
 values('HDB01', 'S0001', 10, 6000);
 
 insert into
 CTHoaDonBan(MaHDBan, MaSach, Sluong, GiaTien)
+values('HDB02', 'S0001', 1, 6000);
+
+insert into
+CTHoaDonBan(MaHDBan, MaSach, Sluong, GiaTien)
 values('HDB01', 'S0001', 5, 6000);
 
---Kho
 insert into
 Kho(MaKho, MaHDNhap, MaSach, Sluong, create_at)
 values('K01', 'HDN01', 'S0001', 1000, CURRENT_TIMESTAMP)
+
+SELECT *
+FROM NhanVien
+
+select HoTen, MaHDBan, TongTien
+from NhanVien nv join HoaDonBan hdb on nv.MaNV = hdb.MaNV
+
+go 
+
+select hdb.MaHDBan, MaSach, cthdb.Sluong, cthdb.GiaTien, cthdb.GiaTien * cthdb.Sluong as TongTien
+from HoaDonBan hdb join CTHoaDonBan cthdb on hdb.MaHDBan = cthdb.MaHDBan
+
+select TenSach, TenNXB, DonGia, HinhAnh
+from Sach S join NhaXuatBan N on S.MaNXB = N.MaNXB
+
+select *
+from NguoiDung
+
+select *
+from GioHang
+
+select *
+from Kho
