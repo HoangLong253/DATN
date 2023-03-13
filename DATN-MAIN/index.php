06ec98e71f0d4222eaac807cbdac6a1e6c8ffec8
HoangLong253 <0306201253@caothang.edu.vn>
2023-03-13 11:52:28 +0700

    thêm trang web + hiệu chỉnh sql

---
+++ DATN-MAIN/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Head -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore</title>
+    <link rel="stylesheet" href="./assets/font/..">
+    <link rel="stylesheet" href="./assets/bootstrap-5.0.2-dist/..">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/fonts.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-6.3.0-web/css/all.min.css">
+
+</head>
+<!-- Body -->
+
+<body>
+    <div class="main">
+        <!-- Header -->
+        <div class="header">
+            <div class="wrap-content">
+                <div class="header__navbar">
+                    <ul class="header__navbar-list">
+                        <a href="https://bom.so/pPF8sq" class="header__navbar-item header__navbar-item--separate ">
+                            <i class="header__navbar-icon fa-solid fa-location-dot"></i>
+                            125/2 Hoà Hưng, P12, Q10, TP HCM
+                        </a>
+
+                        <li class="header__navbar-item">
+                            <span class="header__navbar-title--no-pointer">Kết nối</span>
+                            <a href="https://bom.so/tyvGGr" class="header__navbar-icon-link">
+                                <i class="header__navbar-icon fa-brands fa-facebook"></i>
+                            </a>
+                            <a href="" class="header__navbar-icon-link">
+                                <i class="header__navbar-icon fa-brands fa-square-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="header__navbar-list">
+                        <li class="header__navbar-item">
+                            <a href="" class="header__navbar-item-link">
+                                <i class="header__navbar-icon fa-solid fa-bell"></i>
+                                Thông Báo
+                            </a>
+                            <!--<div class="header__notify">
+                                <header class="header__notify-header">
+                                    <h3>Thông báo mới nhận</h3>
+                                </header>
+                                <ul class="header__notify-list">
+                                    <li class="header__notify-item header__notify-item--viewed">
+                                        <a class="header__notify-link" href="">
+                                            <span> <img class="header__notify-img" src="./assets/images/logo.png" alt="" class="header__notify-img">
+                                            </span>
+                                            <div class="header__notify-infor">
+                                                <span class="header__notify-name">Mắt Biếc Xuất Bản 2019</span>
+                                                <span class="header__notify-desc">Tác giả Nguyễn Nhật Ánh...</span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="header__notify-item header__notify-item--viewed">
+                                        <a class="header__notify-link" href="">
+                                            <span> <img class="header__notify-img" src="./assets/images/logo.png" alt="" class="header__notify-img">
+                                            </span>
+                                            <div class="header__notify-infor">
+                                                <span class="header__notify-name">Mắt Biếc Xuất Bản 2019</span>
+                                                <span class="header__notify-desc">Tác giả Nguyễn Nhật Ánh...</span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="header__notify-item header__notify-item--viewed">
+                                        <a class="header__notify-link" href="">
+                                            <span> <img class="header__notify-img" src="./assets/images/logo.png" alt="" class="header__notify-img">
+                                            </span>
+                                            <div class="header__notify-infor">
+                                                <span class="header__notify-name">Mắt Biếc Xuất Bản 2019</span>
+                                                <span class="header__notify-desc">Tác giả Nguyễn Nhật Ánh...</span>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <div class="header__notify-footer">
+                                    <a href="" class="header__notify-footer-btn">Xem tất cả</a>
+                                </div>
+                            </div>-->
+                        </li>
+                        <li class="header__navbar-item">
+                            <a href="" class="header__navbar-item-link">
+                                <i class="header__navbar-icon fa-solid fa-circle-question"></i>
+                                Trợ Giúp
+                            </a>
+                        </li>
+                        <li class="header__navbar-item header__navbar-item-bold header__navbar-item--separate">Đăng Ký
+                        </li>
+                        <li class="header__navbar-item header__navbar-item-bold">Đăng Nhập</li>
+                    </ul>
+                </div>
+                <div class="header__main">
+                    <div class="header__main-logo">
+                        <img class="header__main-logo-img " src="./assets/images/logo.png" alt="">
+                    </div>
+                    <div class="header__main-cpnname">
+                    <a href="read.php" class="header__navbar-icon-link">
+                        <i class="header__navbar-icon fa-brands fa-square-instagram"></i>
+                    </a>
+                    </div>
+                    <div class="header__main-search">
+                        
+                    </div>
+                    <div class="header__main-cart">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Containter -->
+        <div class="container">
+            
+        </div>
+        <!-- Fopoter -->
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+
+</html>
