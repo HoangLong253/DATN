3a9313b3fbbf672db14b247b0770b2a32361d144
HoangLong253 <0306201253@caothang.edu.vn>
2023-03-16 16:52:27 +0700

    add thêm nút cho admin

--- web/DATN-MAIN/admin/index.php
+++ web/DATN-MAIN/admin/index.php
@@ -2,16 +2,33 @@
 <html>
     <head>
         <title>Admin</title>
-        <link rel="stylesheet" href="admin\css\style.css">
+        <!--<link rel="stylesheet" href="admin\css\style.css">-->
         <link rel="stylesheet" href="admin\css\base.css">
         <link rel="stylesheet" href="admin\css\fonts.css">
+        <link rel="stylesheet" href="admin\css\mau.css">
     </head>
     <body>
         <div class="wrapper">
-            <aside class="main-aside">
-                hi
+            <aside class="main-sidebar sidebar-dark-primary elevation-4 text-sm">
+                <a class="brand-link" href="http://localhost:60/admin">
+                    <img class="brand-image" src="./assets/images/logo.png" alt="">
+                    <div class="header__cpnname">
+                        Trang Admin
+                    </div>
+                </a>
+            <a class="nav-link btn " href="books.php">
+                Sách
+            </a>
+            <br>
+            <a class="nav-link btn" href="employees.php">
+                Nhân Viên
+            </a>
+            <br>
+            <a class="nav-link btn" href="users.php">
+                Người dùng
+            </a>
             </aside>
-            <div class="content-wrapper right-content">
+            <div class="content-wrapper ">
 
             </div>
         </div>
