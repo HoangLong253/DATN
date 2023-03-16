b71e1844c4ebec9fb6a5d11bf2ec05f903dcf69f
HoangLong253 <0306201253@caothang.edu.vn>
2023-03-16 13:18:50 +0700

    sửa phần admin

---
+++ web/DATN-MAIN/admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin</title>
+        <link rel="stylesheet" href="admin\css\style.css">
+        <link rel="stylesheet" href="admin\css\base.css">
+        <link rel="stylesheet" href="admin\css\fonts.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <aside class="main-aside">
+                hi
+            </aside>
+            <div class="content-wrapper right-content">
+
+            </div>
+        </div>
+    </body>
+</html>
